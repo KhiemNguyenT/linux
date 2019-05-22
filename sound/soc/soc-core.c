daemory/rcar

ASoC: Modify check condition of multiple bindings of components
https://patchwork.kernel.org/patch/7385501/
...and some more hacks to bind one component (with several DAIs)
to more than one sound card.

KF has 4 sound cards (pcm3168a, ak4613, radio, wl18xx) and just one
compinent ec500000.sound that can not be bound to all 4 cards.
This is a lack of current implementation of sound/soc/sh/rcar/* ASoC stack
The ec500000.sound resources (PCM/DMA, dais) needs to be shared between
all 4 sound cards if we want all cards work runtime.
Or we have to enable only one of them in dts file as it is designed.

Signed-off-by: Andrey Gusakov <andrey.gusakov@cogentembedded.com>

sound/soc/soc-core.c
@@ -1418,7 +1418,8 @@ static int soc_probe_component(struct snd_soc_card *card,
 		return 0;
 
 	if (component->card) {
-		if (component->card != card) {
+		if (component->card != card &&
+		    component->registered_as_component) {
 			dev_err(component->dev,
 				"Trying to bind component to card \"%s\" but is already bound to card \"%s\"\n",
 				card->name, component->card->name);
@@ -3261,7 +3262,8 @@ int snd_soc_register_component(struct device *dev,
 		goto err_free;
 
 	cmpnt->ignore_pmdown_time = true;
-	cmpnt->registered_as_component = true;
+	if (num_dai == 1)
+		cmpnt->registered_as_component = true;
 
 	ret = snd_soc_register_dais(cmpnt, dai_drv, num_dai, true);
 	if (ret < 0) {

KhiemNguyenT/linux

sound/soc/soc-core.c
@@ -1516,7 +1516,8 @@ static int soc_probe_component(struct snd_soc_card *card,
 		return 0;
 
 	if (component->card) {
-		if (component->card != card) {
+		if (component->card != card &&
+		    component->registered_as_component) {
 			dev_err(component->dev,
 				"Trying to bind component to card \"%s\" but is already bound to card \"%s\"\n",
 				card->name, component->card->name);
@@ -3471,7 +3472,8 @@ int snd_soc_add_component(struct device *dev,
 		goto err_free;
 
 	component->ignore_pmdown_time = true;
-	component->registered_as_component = true;
+	if (num_dai == 1)
+		component->registered_as_component = true;
 
 	if (component_driver->endianness) {
 		for (i = 0; i < num_dai; i++) {